a02a3dc1a6ae14a80fb2efbf36c114e8f413230f
yasmin-vitoria-ferreira <yasmin.v.ferreira12@aluno.senai.br>
2026-08-19 10:59:08 -0300

    Alterações aula de TODOLIST, 19/08

--- src/todolist/ToDoList.java
+++ src/todolist/ToDoList.java
@@ -4,17 +4,92 @@
  */
 package todolist;
 
-/**
- *
- * @author Aluno
- */
-public class ToDoList {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class ToDoList{
     
+    public static void main(String[] args) {
+     
+        Scanner scanner = new Scanner(System.in);
+        
+        String[] tarefas = new String[10];
+        boolean[] concluidas = new boolean[10];
+        int opcao = 0;
+        
+        while(opcao != 5){
+            
+            System.out.println("\n=== LISTA DE TAREFA ===");
+            System.out.println("1 - Adicionar Tarefa");
+            System.out.println("2 - Listar Tarefa");
+            System.out.println("3 - Concluir Tarefa");
+            System.out.println("4 - Excluir Tarefa");
+            System.out.println("5 - Sair");
+        
+            System.out.print("\nDigite o numero corespondente a opcao desejada: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch(opcao){
+                
+                case 1 -> {
+                    int posicaoLivre = -1;
+                    
+                    for(int i = 0; i < tarefas.length; i++){
+                        if(tarefas[i] == null){
+                            posicaoLivre = i;
+                        }
+                    }
+                    if(posicaoLivre == -1){
+                        System.out.println("A lista de tarefas está cheia");
+                    }else{
+                        System.out.println("Digite a tarefa: ");
+                        tarefas[posicaoLivre] = scanner.nextLine();
+                        concluidas[posicaoLivre] = false;
+                        System.out.println("Tarefa adicionada com sucesso!");
+                    }
+                }
+                    
+                case 2 -> {
+                    System.out.println("\n=== MINHAS TAREFAS QUERIDAS :3 ===");
+                    
+                    boolean existeTarefa = false;
+                    
+                    for(int i = 0; i < tarefas.length; i++){
+                        if(tarefas[i] != null){
+                            existeTarefa = true;
+                            
+                            if(concluidas[i] == true){
+                                System.out.println((i + 1) + " - [x] " + tarefas[i]);
+                            } else{
+                                System.out.println((i + 1) + " - [ ] " + tarefas[i]);
+                            }
+                        }
+                    }
+                    if(existeTarefa == false){
+                        System.out.println("Nenhuma tarefa existente sua anta");
+                    }
+                }
+                    
+                case 3 -> {
+                    System.out.println("Digite o numero da tarefa que deseja concluir: ");
+                    int concluirTarefa = scanner.nextInt();
+                    
+                    int indicieConcluir = concluirTarefa - 1;
+                    
+                    if(indicieConcluir >= 0 && indicieConcluir < tarefas.length && tarefas[indicieConcluir] != null){
+                        
+                        concluidas[indicieConcluir] = true;
+                        
+                        System.out.println("\nTarefa concluida com sucesso");
+                        
+                    }else{
+                        System.out.println("\nTarefa invalida, pobre");
+                    }
+                }
+                    
+                case 4 -> {
+                }
+            }
+        }
+    }
 }
